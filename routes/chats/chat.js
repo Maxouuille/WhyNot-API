60ffdf51428dc47976a3bf20efd48a0af4cb14f2
test <maxime.i@hotmail.com>
2019-09-02 11:05:41 +0200

    Ajout du chat

--- routes/chats/chat.js
+++ routes/chats/chat.js
@@ -11,6 +11,22 @@ const {ObjectId} = require('../../config.js');
 const {dateNow} = require('../../config');
 const {upload} = require('../../config');
 
+router.get('/chat/:id', verifyToken, async (req, res, next) => {
+    const client = new MongoClient(MONGODB_URI, {useNewUrlParser: true});
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const col = db.collection('chat');
+        let result = await col.find({ $or: [{emailSender: req.query.id}, {emailReciver: req.query.id} ] }).toArray();
+        res.send({
+            chat: result,
+            error: null
+        });
+    } catch (err) {
+        res.send(err);
+    }
+    client.close();
+});
 
 router.get('/', verifyToken, async (req, res, next) => {
     const client = new MongoClient(MONGODB_URI, {useNewUrlParser: true});
@@ -29,22 +45,7 @@ router.get('/', verifyToken, async (req, res, next) => {
     client.close();
 });
 
-router.get('/:id', verifyToken, async (req, res, next) => {
-    const client = new MongoClient(MONGODB_URI, {useNewUrlParser: true});
-    try {
-        await client.connect();
-        const db = client.db(dbName);
-        const col = db.collection('chat');
-        let result = await col.find({ $or: [{emailSender: req.query.id}, {emailReciver: req.query.id} ] }).toArray();
-        res.send({
-            chat: result,
-            error: null
-        });
-    } catch (err) {
-        res.send(err);
-    }
-    client.close();
-});
+
 
 
 module.exports = router;
